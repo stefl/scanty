9435ec1438ff1342c14c5a5ebe7e096a7e823a62
Mark Sands <marksands07@gmail.com>
2010-04-12 05:31:28 -0500

    added option to load config vars from local yaml file

--- lib/post.rb
+++ lib/post.rb
@@ -1,9 +1,9 @@
-require File.dirname(__FILE__) + '/../vendor/maruku/maruku'
+require 'maruku'
 
 $LOAD_PATH.unshift File.dirname(__FILE__) + '/../vendor/syntax'
 require 'syntax/convertors/html'
 
-set :database, (ENV['DATABASE_URL'] || 'sqlite://blog.db')
+set :database, ENV['DATABASE_URL'] || 'sqlite://blog.db'
 
 migration "create posts table" do
   database.create_table :posts do
